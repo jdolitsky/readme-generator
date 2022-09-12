9b3df86c6df52eb46d7e1612f88043704b1453c2
Josh Dolitsky <josh@dolit.ski>
2022-09-12 17:30:49 -0500

    Integrate with crane to list tags, get digests
    Signed-off-by: Josh Dolitsky <josh@dolit.ski>

--- README.md
+++ README.md
@@ -8,5 +8,6 @@ A README generator for Chainguard Images
 go run main.go \
   -name nginx \
   -location distroless.dev/nginx \
+  -exclude-tags 1.20.2,1.20,1.23.0 \
   > README-GENERATED.md
 ```
--- main.go
+++ main.go
@@ -6,8 +6,11 @@ import (
 	"flag"
 	"fmt"
 	"os"
+	"sort"
 	"strings"
 	"text/template"
+
+	"github.com/google/go-containerregistry/pkg/crane"
 )
 
 const RekorSearchRootURL = "https://rekor.tlog.dev"
@@ -53,7 +56,7 @@ func check(err error) {
 }
 
 func main() {
-	input, err := getInputFromCommandLine()
+	input, err := buildTemplateInput()
 	check(err)
 	var buf bytes.Buffer
 	err = readmeTemplate.Execute(&buf, input)
@@ -61,29 +64,82 @@ func main() {
 	fmt.Println(buf.String())
 }
 
-func getInputFromCommandLine() (*ReadmeTemplateData, error) {
-	name := flag.String("name", "", "location of the image")
+func buildTemplateInput() (*ReadmeTemplateData, error) {
+	name := flag.String("name", "", "name of the image")
 	location := flag.String("location", "", "location of the image")
+	excludeTags := flag.String("exclude-tags", "", "comma-separated list of tags to exlcude (optional)")
 	flag.Parse()
 	if *name == "" || *location == "" {
 		flag.Usage()
 		return nil, fmt.Errorf("please provide required command-line flags")
 	}
+
+	tags, err := getTags(*location, strings.Split(*excludeTags, ","))
+	if err != nil {
+		return nil, err
+	}
+
 	input := ReadmeTemplateData{
 		Name:     *name,
 		Location: *location,
-		Tags: []ReadmeTemplateDataTag{
-			{
-				Aliases:  []string{"a", "b", "c"},
-				Digest:   "sha256:abc",
-				RekorURL: fmt.Sprintf("%s/?digest=sha256:abc", RekorSearchRootURL),
-			},
-			{
-				Aliases:  []string{"x", "y", "z"},
-				Digest:   "sha256:xyz",
-				RekorURL: fmt.Sprintf("%s/?digest=sha256:xyz", RekorSearchRootURL),
-			},
-		},
+		Tags:     tags,
 	}
 	return &input, nil
+}
+
+func getTags(location string, excluded []string) ([]ReadmeTemplateDataTag, error) {
+	allTags, err := crane.ListTags(location)
+	if err != nil {
+		return nil, err
+	}
+
+	// Filter out:
+	// 1. cosign-related tags
+	// 2. tags produced by apko-snapshot action
+	// 3. tags explicitly to be excluded
+	filteredTags := []string{}
+	for _, tag := range allTags {
+		if strings.HasPrefix(tag, "sha256-") || strings.HasPrefix(tag, "latest-") || stringInSlice(tag, excluded) {
+			continue
+		}
+		filteredTags = append(filteredTags, tag)
+	}
+
+	// Check the digest for each tag, and collapse them into the digest they point to
+	digests := map[string][]string{}
+	for _, tag := range filteredTags {
+		r := fmt.Sprintf("%s:%s", location, tag)
+		key, err := crane.Digest(r)
+		if err != nil {
+			return nil, fmt.Errorf("unable to determine digest for %s: %w", r, err)
+		}
+		if val, ok := digests[key]; !ok {
+			digests[key] = []string{tag}
+		} else {
+			digests[key] = append(val, tag)
+		}
+	}
+
+	// Finally, build out the template data structure
+	readmeTemplateDataTags := []ReadmeTemplateDataTag{}
+	for digest, aliases := range digests {
+		sort.Strings(aliases)
+		readmeTemplateDataTags = append(readmeTemplateDataTags, ReadmeTemplateDataTag{
+			Aliases:  aliases,
+			Digest:   digest,
+			RekorURL: fmt.Sprintf("%s/?hash=%s", RekorSearchRootURL, digest),
+		})
+	}
+
+	return readmeTemplateDataTags, nil
+}
+
+// https://stackoverflow.com/a/15323988
+func stringInSlice(a string, list []string) bool {
+	for _, b := range list {
+		if b == a {
+			return true
+		}
+	}
+	return false
 }
